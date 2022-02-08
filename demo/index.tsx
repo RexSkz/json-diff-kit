0f83112f84fcd037df9a04581d10df69aa2e1e0b
Rex Zeng <xianwen.zeng@shopee.com>
2022-02-09 00:03:50 +0800

    v1.0.0

--- demo/index.tsx
+++ demo/index.tsx
@@ -140,7 +140,7 @@ const Demo: React.FC = () => {
       <div className="demo-footer">
         <p>Made with ♥ by Rex Zeng</p>
       </div>
-      <a href="https://github.com/tuanchauict/Repo-stars-image">
+      <a href="https://github.com/rexskz/json-diff-kit">
         <img
           style={{ position: 'absolute', top: 0, right: 0, border: 'none' }}
           src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
