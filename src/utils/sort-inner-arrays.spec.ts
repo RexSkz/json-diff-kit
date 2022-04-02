c48ed37a8e2126d6759741b6f20947b7d13dc207
Rex Zeng <rex@rexskz.info>
2022-04-03 00:45:12 +0800

    test: write some basic tests for utils

---
+++ src/utils/sort-inner-arrays.spec.ts
@@ -0,0 +1,49 @@
+import sortInnerArrays from './sort-inner-arrays';
+
+describe('Utility function: sortInnerArrays', () => {
+
+  it('should return the original value if value is not array or object', () => {
+    expect(sortInnerArrays(1)).toBe(1);
+    expect(sortInnerArrays('2')).toBe('2');
+    expect(sortInnerArrays(true)).toBe(true);
+  });
+
+  it('should work like `Array.prototype.sort` for one-level typed arrays', () => {
+    const numArray = Array(100).fill(0).map(() => Math.random() * 1e6 | 0);
+    const sortedNumArray = [...numArray].sort((a, b) => a - b);
+    expect(sortInnerArrays(numArray)).toStrictEqual(sortedNumArray);
+
+    const strArray = Array(100).fill(0).map(() => String(Math.random() * 1e6 | 0));
+    const sortedStrArray = [...strArray].sort((a, b) => a.localeCompare(b));
+    expect(sortInnerArrays(strArray)).toStrictEqual(sortedStrArray);
+  });
+
+  it('should sort all the levels for a nested array', () => {
+    // array as an array item
+    {
+      const arr = Array(100).fill(0).map(() => Math.random() * 1e6 | 0);
+      const sortedArr = [...arr].sort((a, b) => a - b);
+      expect(sortInnerArrays(arr)).toStrictEqual(sortedArr);
+    }
+
+    // array as an object value
+    {
+      const arr = Array(100).fill(0).map(() => Math.random() * 1e6 | 0);
+      const sortedArr = [...arr].sort((a, b) => a - b);
+      expect(sortInnerArrays(arr)).toStrictEqual(sortedArr);
+    }
+  });
+
+  it('should work properly for one-level mix-typed arrays', () => {
+    const arr = [1, '2', true, '3', null, [], -1, false, null, '0', {}];
+    const sortedArr = [false, true, -1, 1, '0', '2', '3', null, null, [], {}];
+    expect(sortInnerArrays(arr)).toStrictEqual(sortedArr);
+  });
+
+  it('should work properly for nested mix-typed arrays', () => {
+    const arr = [1, { t: 6 }, [{ a: 1, b: ['2', 7] }, 4], null, [], -1, null, '0', {}];
+    const sortedArr = [-1, 1, '0', null, null, [4, { a: 1, b: [7, '2'] }], [], { t: 6 }, {}];
+    expect(sortInnerArrays(arr)).toStrictEqual(sortedArr);
+  });
+
+});
--- src/utils/sort-inner-arrays.ts
+++ src/utils/sort-inner-arrays.ts
@@ -1,3 +1,54 @@
+/**
+ * The compare function to correct the order for "array" or "object":
+ * - The order for 2 values with different types are: boolean, number, string, null, array, object.
+ * - The order for 2 values with the same type is according to the type:
+ *   - For boolean, number, string: use the `<` sign.
+ *   - For array and object: preserve the original order (or do we have a better idea?)
+ */
+
+const getOrderByType = (value: any) => {
+  if (typeof value === 'boolean') {
+    return 0;
+  }
+  if (typeof value === 'number') {
+    return 1;
+  }
+  if (typeof value === 'string') {
+    return 2;
+  }
+  if (value === null) {
+    return 3;
+  }
+  if (Array.isArray(value)) {
+    return 4;
+  }
+  if (typeof value === 'object') {
+    return 5;
+  }
+};
+
+const cmp = (a: any, b: any) => {
+  const orderByTypeA = getOrderByType(a);
+  const orderByTypeB = getOrderByType(b);
+
+  if (orderByTypeA !== orderByTypeB) {
+    return orderByTypeA - orderByTypeB;
+  }
+
+  if (a === null && b === null || Array.isArray(a) && Array.isArray(b) || orderByTypeA === 5 && orderByTypeB === 5) {
+    return 0;
+  }
+
+  switch (typeof a) {
+    case 'number':
+      return a - b;
+    case 'string':
+      return a.localeCompare(b);
+    case 'boolean':
+      return (+a) - (+b);
+  }
+};
+
 const sortInnerArrays = (source: any) => {
   if (!source || typeof source !== 'object') {
     return source;
@@ -5,12 +56,7 @@ const sortInnerArrays = (source: any) => {
 
   if (Array.isArray(source)) {
     const result = [...source];
-    result.sort((a, b) => {
-      if (typeof a === 'number' && typeof b === 'number') {
-        return a - b;
-      }
-      return String(a).localeCompare(String(b));
-    });
+    result.sort((a, b) => cmp(a, b));
     return result.map(item => sortInnerArrays(item));
   }
 
